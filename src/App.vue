11084a951a48196b868006e9d811d30e35fb763a
Giotreglia <giovanni.treglia@gmail.com>
2023-03-31 14:29:54 +0200

    Milestone 0

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,24 @@
 <script>
+import axios from 'axios';
+import { store } from './store';
+import MyHeader from './components/MyHeader.vue';
+
+export default {
+  components: {
+    MyHeader
+  },
+
+  data() {
+    return {
+      store
+    }
+  }
+}
 </script>
 
-<template></template>
+<template>
+  <MyHeader />
+</template>
 
 <style lang="scss">
 @use './styles/general.scss';
---
+++ src/components/MyHeader.vue
@@ -0,0 +1,37 @@
+<script>
+export default {
+    name: 'MyHeader'
+}
+</script>
+
+<template>
+    <header>
+        <div id="logo-container">
+            <h1 id="logo">Boolflix</h1>
+        </div>
+
+        <div id="search-container">
+            <input type="text" placeholder="Search">
+            <button>Search</button>
+        </div>
+    </header>
+</template>
+
+<style lang="scss">
+@use '../styles/general.scss';
+@use '../styles/partials/variables' as *;
+
+header {
+    height: 100px;
+    background-color: $color-secondary;
+    display: flex;
+    justify-content: space-between;
+    padding: 10px 30px;
+    align-items: center;
+}
+
+#logo {
+    color: $color-primary;
+    font-size: 60px;
+}
+</style>
